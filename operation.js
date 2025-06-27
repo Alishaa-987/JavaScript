01f1eee7a56861914f2e1e1e3abd2b0db880f86c
Alisha fatima <alishafatima6768@gmail.com>
2025-06-27 05:25:05 +0000

    operation

---
+++ operation.js
@@ -0,0 +1,10 @@
+// ************ OPERATION ****************
+console.log(1 + "3")  // answer is 13
+console.log("1" + 3)   // answer is 13
+
+// Thats treat as a string because the first precedence number is string so it not add just treat the number as stirng and concate that 
+
+console.log( 1 + 2)  // thats tbe simple concate mean addition in numbers 
+console.log("1" + "2")  // that complete stirng bhevior its output is 12
+console.log( "1"  + 2  + 2)  // thats also treat as stirng because in that fist one is string so ouptu is 122
+console.log( 1 + 2 + "3")   // in that fist one is number so it treat them like that and in ouput hte answr is 33
