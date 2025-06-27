e0e4616a34c2bb707628b6df7736673b3ec690bb
Alisha fatima <alishafatima6768@gmail.com>
2025-06-27 06:52:01 +0000

    memory

---
+++ basics/memory.js
@@ -0,0 +1,16 @@
+// There are two types of memory
+// Stack =>  this memory is used in primitive data type
+// In that memory we get copy of data 
+
+
+// Heap  => THis memory is used in non primitive data type
+// In that we get the refernce of data
+
+
+
+
+/*
+Stack me ap ko memory ke copy milte he ap origna vlaue chage nahi kar sakte 
+
+Heap me ap  ko memory ka refrence dete hee matlab agr ap koi bhe value change karte hee to to wo origanl vlaue me bhe change hote hee
+*/
