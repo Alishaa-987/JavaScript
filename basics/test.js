e237dd2da8470f753b23959f8b73e88404c7d558
Alisha fatima <alishafatima6768@gmail.com>
2025-06-27 04:56:13 +0000

    number

--- basics/test.js
+++ basics/test.js
@@ -1,1 +1,37 @@
-console.log("alisha") ;
+
+// varaible decleration
+/*
+ var ->used for variable decleration and and its scope is unlimited 
+let -> scope is limited to function block but it dont allow to update its varaible value
+const-> this is used for permanently decleration of a variable value 
+*/
+
+
+//  DATA TYPES 
+/*
+string
+number
+boolean 
+undefined -> its datatype is also undefined . Its show on output when we dont declare the value of a variable
+NUll -> it type is object . Thats a value which is 0
+*/
+
+//DATA TYPE CONVERSION
+/*
+ Sometime we want to convert our data into another datatype and we can do it by easily like
+ let age = 18
+ console.log(typeof(age))
+ In output it give use number
+
+ suppose i want to convert that into string so thats how i do it
+ let agee = "18"    
+ now in that case it give us the string 
+ so we can covert that in to the another datayep
+ let value = Number(agee)
+ console.log(typeof(agee))
+*/
+
+let age = "34"
+console.log(typeof( age))
+let value = Number(age)
+console.log(typeof value)
