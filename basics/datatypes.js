c1f107d08d5e906f8e1c1fec98f20e969a3f46ae
Alisha fatima <alishafatima6768@gmail.com>
2025-06-27 06:42:06 +0000

    datatypes

---
+++ basics/datatypes.js
@@ -0,0 +1,35 @@
+// There are two types of data types
+//  primitive data types
+// Non primitive data types
+
+//  There are 7 types of primitive data types 
+/*
+String   ,  Number   , Boolean  , Null  , Undefined  , symbol   , BigInt
+wo data 
+
+Refrence (Non primitve data type )
+in ka refrence memory me direct allocate kia jata hee
+Array  ,  Object  ,  Function 
+
+Non primitve data type ka  mostly function ata hee 
+
+*/
+
+const id = Symbol('123')
+const secondId = Symbol('123')
+console.log(id === secondId)   // the asner is false 
+
+
+1) Primitive Datatypes
+       Number => number
+       String  => string
+       Boolean  => boolean
+       null  => object
+       undefined  =>  undefined
+       Symbol  =>  symbol
+       BigInt  =>  bigint
+
+2) Non-primitive Datatypes
+       Arrays  =>  object
+       Function  =>  function
+       Object  =>  object
