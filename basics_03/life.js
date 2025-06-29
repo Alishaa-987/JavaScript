7e9d51e8389506861e7c4f00d7e02c1a0054d169
Alisha fatima <alishafatima6768@gmail.com>
2025-06-29 22:22:55 +0000

    life

---
+++ basics_03/life.js
@@ -0,0 +1,18 @@
+// life 
+// jab hmm function ko foran se execute karvana chah rahe hoo jaise he wo run karee jaise ke database ka connection to tab hmm ye use karte hee
+
+// Immediately Invoked Function Expressions (IIFE)
+
+
+(function chai(){
+    // named IIFE
+    console.log(`DB CONNECTED`);
+})();                                    // simply brackets on whole function and then for execution put brackets 
+
+
+
+// in case of arrwo function we write it like that and also give parameters
+
+( (name) => {
+    console.log(`DB CONNECTED TWO ${name}`);
+} )('hitesh')
